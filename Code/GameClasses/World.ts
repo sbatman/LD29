ed27f5c13639166759762aabe612b2cda54770dc
sbatman <steven@insanedev.co.uk>
2014-04-26 16:44:25 +0100

    Added the ability to build the world from a csv file

--- Code/GameClasses/World.ts
+++ Code/GameClasses/World.ts
@@ -9,20 +9,15 @@ module LD29
     export class World
     {
         MasterMap: Phaser.Tilemap;
-        MainTileMap_L1: Phaser.Tilemap;
+        MasterMap_L1: Phaser.TilemapLayer;
+       // MainTileMap_L1: Phaser.Tilemap;
         constructor(game: Phaser.Game)
         {
-            this.MasterMap = game.add.tilemap(null);
+            this.MasterMap = game.add.tilemap("content-data-world-main",32,32);
             this.MasterMap.addTilesetImage("content-graphics-world-dungeontiles");
-
-            this.MainTileMap_L1 = this.MasterMap.create("L12", 100, 100, 32, 32);
-            for (var x = 0; x < 50; x++)
-            {
-                for (var y = 0; y < 50; y++)
-                {
-                    this.MasterMap.putTile(0, x, y, this.MainTileMap_L1);
-                }
-            }
+            this.MasterMap_L1 = this.MasterMap.createLayer(0);
+            this.MasterMap_L1.resizeWorld();
+           
         }
     }
 }
